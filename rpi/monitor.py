c9f89d6858c4bfbac2a01199a7f29a7ae3985f6c
Algirdas Č <18305296+algirdasc@users.noreply.github.com>
2021-11-08 19:09:32 +0200

    Update monitor.py

--- rpi/monitor.py
+++ rpi/monitor.py
@@ -12,49 +12,26 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 
-DISPLAY_ACTIVE = False
-DISPLAY_TIMEOUT = 3
-ASC2PS2 = {
-    '1'         : bytearray(b'\x16\xF0\x16'),
-    '2'         : bytearray(b'\x1E\xF0\x1E'),
-    'HOME'      : bytearray(b'\xE0\x6C\xE0\xF0\x6C'),
-    'END'       : bytearray(b'\xE0\x69\xE0\xF0\x69'),
-    'UP'        : bytearray(b'\xE0\x75\xE0\xF0\x75'),
-    'DOWN'      : bytearray(b'\xE0\x72\xE0\xF0\x72'),
-    'LEFT'      : bytearray(b'\xE0\x6B\xE0\xF0\x6B'),
-    'RIGHT'     : bytearray(b'\xE0\x74\xE0\xF0\x74'),
-    'ENTER'     : bytearray(b'\x5A\xF0\x5A'),
-    'ESC'       : bytearray(b'\x76\xF0\x76'),
-}
+DISPLAY_ACTIVE_AT = time.time()
+DISPLAY_UP = False
+DISPLAY_UP_TIMEOUT = 300
 
-def send_ps2(ser, sequence):    
-    l = int(len(sequence))
-    f0c = 0
-    for i in range(l):
-        scancode = sequence[i]
-        print(scancode)
-        if scancode == 0xF0:
-           time.sleep(0.01)
-           f0c = 2
-        ser.write(bytearray([scancode]))
-        if f0c > 0:
-           f0c -= 1
-           if f0c == 0:
-               time.sleep(0.01)
+ANDROID_ACTIVE = False
+
 
 def main() -> None:
 
-    ps2 = serial.Serial(port='/dev/ttyVB00', baudrate=9600, parity=serial.PARITY_NONE, timeout=5)
-
     _thread.start_new_thread(usb_monitor, ())
-    _thread.start_new_thread(serial_monitor, (ps2))
+    _thread.start_new_thread(serial_monitor, ())
 
     while True:
-        pass
+        time.sleep(1)
 
-def serial_monitor(ps2) -> None:
+def serial_monitor() -> None:
+    global DISPLAY_UP, DISPLAY_ACTIVE_AT, DISPLAY_UP_TIMEOUT
+
     logging.info('Starting serial monitor thread')
-    last_display_state_ts = time.time()
+    LAST_ANDROID_ACTIVE = ANDROID_ACTIVE
 
     ser = None
 
@@ -66,13 +43,13 @@ def serial_monitor(ps2) -> None:
                     logging.debug('Closing serial')
                     ser.close()
                     ser = None
-                logging.debug('Waiting 30 seconds')
-                time.sleep(30)
+                logging.debug('Waiting 5 seconds')
+                time.sleep(5)
                 continue
 
-            if ser is None:                
+            if ser is None:
                 logging.debug('Opening serial')
-                ser = serial.Serial(port='/dev/serial0', baudrate=115200, timeout=1)
+                ser = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=1)
                 logging.info('Opened serial')
 
             line = ser.readline().strip().decode('utf-8')
@@ -80,36 +57,55 @@ def serial_monitor(ps2) -> None:
             if line != '':
                 logging.debug('Serial received: {0}'.format(line))
 
+            if line == 'EVENT_RPI_REBOOT':
+                subprocess.check_call('sudo shutdown -r now', shell=True)
+
             if line == 'EVENT_RPI_SHUTDOWN':
-                # serial_write(ser, 'DISPLAY_DOWN')
                 subprocess.check_call('sudo shutdown -h now', shell=True)
-                exit()
 
             if line == 'EVENT_KEY_UP':
-                send_ps2(ps2, ASC2PS2['UP'])
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key Up', shell=True)
+                continue
 
             if line == 'EVENT_KEY_DOWN':
-                send_ps2(ps2, ASC2PS2['DOWN'])
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key Down', shell=True)
+                continue
 
             if line == 'EVENT_KEY_LEFT':
-                send_ps2(ps2, ASC2PS2['1'])
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key 1', shell=True)
+                continue
 
             if line == 'EVENT_KEY_RIGHT':
-                send_ps2(ps2, ASC2PS2['2'])
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key 2', shell=True)
+                continue
 
             if line == 'EVENT_KEY_ENTER':
-                send_ps2(ps2, ASC2PS2['ENTER'])
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key Return', shell=True)
+                continue
 
             if line == 'EVENT_KEY_BACK':
-                send_ps2(ps2, ASC2PS2['ESC'])
-                
-            if line == 'EVENT_KEY_BACK_LONG':
-                send_ps2(ps2, ASC2PS2['HOME'])                                             
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key Escape', shell=True)
+                continue
 
-            if time.time() - last_display_state_ts >= DISPLAY_TIMEOUT:
-                serial_write(ser, 'DISPLAY_UP' if DISPLAY_ACTIVE is True else 'DISPLAY_DOWN')
-                last_display_state_ts = time.time()
-                
+            if line == 'EVENT_KEY_BACK_LONG':
+                subprocess.check_call('export DISPLAY=:0.0 && xdotool key h', shell=True)
+                continue
+
+            if LAST_ANDROID_ACTIVE is not ANDROID_ACTIVE and ANDROID_ACTIVE is True:
+               DISPLAY_UP = True
+               serial_write(ser, 'DISPLAY_UP')
+
+            if ANDROID_ACTIVE is True:
+               DISPLAY_ACTIVE_AT = time.time()
+
+            if DISPLAY_UP is True and time.time() - DISPLAY_ACTIVE_AT >= DISPLAY_UP_TIMEOUT:
+               DISPLAY_UP = False
+               serial_write(ser, 'DISPLAY_DOWN')
+
+            LAST_ANDROID_ACTIVE = ANDROID_ACTIVE
+
+            time.sleep(0.01)
+
         except Exception as e:
             ser = None
             logging.error('Exception while reading serial: {0}'.format(e))
@@ -127,8 +123,8 @@ def serial_write(ser: serial, data: str) -> None:
 
 
 def usb_monitor() -> None:
-    global DISPLAY_ACTIVE
-    logging.info('Starting USB monitor thread')    
+    global ANDROID_ACTIVE
+    logging.info('Starting USB monitor thread')
 
     while True:
         try:
@@ -138,11 +134,11 @@ def usb_monitor() -> None:
                 if not line:
                     continue
                 if 'android' in line.lower():
-                    if DISPLAY_ACTIVE is False:
+                    if ANDROID_ACTIVE is False:
                         logging.info('Found android device: {0}'.format(line))
                     android_found = True
-                    break                    
-            DISPLAY_ACTIVE = android_found
+                    break
+            ANDROID_ACTIVE = android_found
         except Exception as e:
             logging.error('Exception while scanning USB: {0}'.format(e))
         finally:
